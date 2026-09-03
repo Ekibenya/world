24c4ea1f08a400a40ce1e0fe82b9773e2558cdfc
Ekibenya <ekibenya@outlook.com>
2026-09-03 02:51:33 +0000

    45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guardian-dragon-art-v40';
+const CACHE = 'guardian-dragon-art-v41';
 const CORE = [
   '/',
   '/index.html',
@@ -6,8 +6,8 @@ const CORE = [
   '/core/res/font/metropolitain.ttf',
   '/core/res/world/interface-shell.css?v=13',
   '/core/res/world/visual-novel.css?v=10',
-  '/core/res/world/app.css?v=28',
-  '/core/res/world/menu-shell.js?v=13',
+  '/core/res/world/app.css?v=29',
+  '/core/res/world/menu-shell.js?v=14',
   '/core/res/world/annals-shell.js?v=16',
   '/core/res/world/world-era-intro.js?v=19',
   '/core/res/world/mvu-shell.js?v=10',
